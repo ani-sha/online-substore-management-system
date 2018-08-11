9b38b9ce2cd74c88ab459ff30f59497552de41e6
Anisha <anishamohanty23@gmail.com>
2018-08-11 20:52:44 +0530

    Login Master Page

---
+++ Site.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SYSTEMS_SUBSTORE
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string pers_no;
+            if (Session["admin"] == null)
+            {
+                Response.Redirect("Default.aspx");
+            }
+            else
+            {
+                pers_no = Session["admin"].ToString();
+                Session["admin"] = pers_no;
+            }
+        }
+    }
+}

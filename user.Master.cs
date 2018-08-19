305a1c2ae821d526479096ee32b95d2804f91d48
Anisha <anishamohanty23@gmail.com>
2018-08-19 20:26:29 +0530

    Master Page for User

---
+++ user.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SYSTEMS_SUBSTORE
+{
+    public partial class user : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string pers_no;
+            if (Session["user"] == null)
+            {
+                Response.Redirect("Default.aspx");
+            }
+            else
+            {
+                pers_no = Session["user"].ToString();
+                Session["user"] = pers_no;
+            }
+        }
+    }
+}
